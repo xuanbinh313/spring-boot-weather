4103e95fe1d1e9346c32bf71bcc7347f7c85bbe7
xuanbinh313 <xuanbinh313@gmail.com>
2024-10-04 00:00:54 +0700

    add environment value

--- src/main/java/com/binhcodev/spring_boot_weather/WeatherService.java
+++ src/main/java/com/binhcodev/spring_boot_weather/WeatherService.java
@@ -1,6 +1,7 @@
 package com.binhcodev.spring_boot_weather;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,17 @@ import com.binhcodev.spring_boot_weather.entities.WeatherResponse;
 @Service
 public class WeatherService {
     private static final Logger logger = LoggerFactory.getLogger(WeatherService.class);
-    private final String API_KEY = "4A2NL9DJF8B8HFCGFWUSPVKA3";
-    private final String apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Vietnam?unitGroup=metric&key=" + API_KEY + "&contentType=json";
+    @Value("${weather.api.key}")
+    private String API_KEY;
+    @Value("${weather.api.url}")
+    private String apiURL;
 
     @Cacheable(value = "WeatherCache", key = "#country")
     public WeatherResponse getWeather(String country) {
         // This log will only appear when a new request is made (i.e., cache miss)
         logger.info("Fetching new data from weather API for city: {}", country);
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<WeatherResponse> response = restTemplate.getForEntity(apiURL, WeatherResponse.class);
+        ResponseEntity<WeatherResponse> response = restTemplate.getForEntity(apiURL + country + "?key=" + API_KEY + "&contentType=json", WeatherResponse.class);
         return response.getBody();
     }
 }
